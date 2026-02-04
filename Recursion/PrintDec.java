5bc62af84ca29d11739612a789ef883373a290f3
Shivtej18 <patilshivtej0918@gmail.com>
2026-02-04 09:28:50 +0530

    Recursion Basics

---
+++ Recursion/PrintDec.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+import java.nio.channels.Pipe.SourceChannel;
+
+class PrintDec{
+
+    public static void Dec(int n){
+        if(n==1){
+            System.out.println(1);
+            return;
+        }
+        System.out.print(n+"");
+        Dec(n-1);
+    }
+
+    public static void main(String args[]){
+        Dec(5);
+    }
+}
